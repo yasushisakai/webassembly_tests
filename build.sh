843966aa6b2dd6e8cc528ae7f213e91bcf3f210f
Yasushi Sakai <ysshski@gmail.com>
2018-01-24 17:14:47 -0500

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/home/yasushisakai/code/emsdk/emscripten/incoming/emcc hello.c -s WASM=1 -o dist/hello.html --shell-file template.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]'
+/home/yasushisakai/code/emsdk/emscripten/incoming/emcc hello.cpp -s WASM=1 -o dist/hello.html --shell-file template.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]'
 
-cp hello.* /var/www/html
+cp dist/* /var/www/html
